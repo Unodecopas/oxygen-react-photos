f83fec36722e728b938af51f86edc43efb755320
Jesus Gallardo <gallardomoralesjesus@gmail.com>
2022-09-07 18:08:33 +0200

    style: fix text link to favorites

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,27 @@
-import { Typography, Link, Box } from '@mui/material'
+import { Typography, Link, Box, AppBar, Toolbar } from '@mui/material'
 import React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
+       <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
       <Typography variant='h6' component='div' sx={{flexGrow:1}}>
         <Link component={RouterLink} to='/' underline='none' color='white' >
           OXYGEN PHOTOS
         </Link>
         
       </Typography>
-      <Typography variant='h6' component='div' sx={{flexGrow:1}}>
+      <Typography variant='h6' component='div'>
         <Link component={RouterLink} to='/favorites' underline='none' color='white'>
-          Favorites
+          My Photos
         </Link>
       </Typography>
-
+      </Toolbar>
+      </AppBar>
+      </Box>
     </header>
   )
 }
